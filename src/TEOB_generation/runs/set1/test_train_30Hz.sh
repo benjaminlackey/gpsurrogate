474c05c919d106a4c2406987824af5f7c0474fe1
Michael Puerrer <Michael.Puerrer@ligo.org>
2017-11-06 16:08:37 +0100

    Update configuration files and shell scripts for runs

---
+++ src/TEOB_generation/runs/set1/test_train_30Hz.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source /home/mpuer/lsc/TEOBBNS/etc/lalsuiterc
+
+export PYTHONPATH=$PYTHONPATH:/home/mpuer/projects/
+
+SDIR=/home/mpuer/projects/gpsurrogate/src/TEOB_generation/
+RDIR=/home/mpuer/projects/gpsurrogate/src/TEOB_generation/runs/set1
+CFG=${RDIR}/test_train_30Hz.json
+LOG=${RDIR}/test_train_30Hz.log
+
+cd ${SDIR}
+mpirun -n 24 Generate_TEOB_TD_MPI.py -o ${CFG} > ${LOG} 2>&1 &
+#mpirun -n 1 Generate_TEOB_TD_MPI.py -o ${CFG} > ${LOG} 2>&1 &
+
